3fb44cca22b8a5857848ded22f15ce9b28d5d532
G.P. Halkes <hg@ghalkes.nl>
2010-05-10 11:49:14 +0200

    Make headers automatically on first build; clean headers on clean

--- rules.mk
+++ rules.mk
@@ -5,7 +5,12 @@ ifndef T3NAME
 $(error T3NAME not defined. See $(lastword $(MAKEFILE_LIST)) for details)
 endif
 
-
 T3PATH:=$(dir $(lastword $(MAKEFILE_LIST)))
+T3HEADERS:=$(foreach NAME,errors api,$(T3NAME)_$(NAME).h)
+
 $(T3NAME)_%.h: $(T3PATH)/src/%.h
 	sed "s/SHARED/`echo '$(T3NAME)' | tr [[:lower:]] [[:upper:]]`/g;s/shared/$(T3NAME)/g" $< > $@
+$(OBJECTS): $(T3HEADERS)
+
+clean::
+	rm -rf $(T3HEADERS) 2> /dev/null
