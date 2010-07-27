d493929641826f80381de000b99633c599fa252e
Gertjan Halkes <hg@ghalkes.nl>
2010-07-27 11:01:21 +0200

    Much improved notice update script

--- update_notices.sh
+++ update_notices.sh
@@ -1,40 +1,50 @@
 #!/bin/bash
 
-#cd `dirname "$0"`
 SCRIPT=`basename "$0"`
-FILES=`hg manifest | egrep -v "^($SCRIPT|testsuite/.*|src/Makefile)$"`
+if ! [ -e .hg ] ; then
+	echo "Please run this script in the top of the mercurial repository"
+	exit
+fi
+FILES=`{ hg manifest | egrep -v "^($SCRIPT|testsuite/.*|src/Makefile)$" ; [ -e .notice_mask ] && { cat .notice_mask ; echo ; cat .notice_mask ; } ; } | sort | uniq -u`
 
+want_update() {
+	ANSWER=x
+	while [ "$ANSWER" != "y" ] && [ "$ANSWER" != "n" ] ; do
+		read -n1 -p "Do you want to update this notice? " ANSWER < /dev/tty
+		echo
+	done
+	[ "$ANSWER" = "y" ]
+}
 
 for f in $FILES ; do
-	if head -n1 $f | egrep -q 'Copyright \(C\)' ; then
-
-		notice="`head -n1 $f | egrep 'Copyright \(C\)' | sed -r 's/^.*([0-9]{4})[^0-9]*$/\1/'`"
-		change="`hg log --no-merges --limit 1 --template '{date|isodate}' $f | egrep -o '^[0-9]+'`"
-
-		if [ "$notice" != "$change" ] ; then
-			echo "Updating Copyright notice for $f:"
-			if head -n1 $f | egrep -q '[0-9]{4}-[0-9]{4}' ; then
-				sed -i -r "1s/([0-9]{4}-)[0-9]{4}/\\1$change/" $f
-			else
-				sed -i -r "1s/([0-9]{4})([^0-9]+)/\\1-$change\\2/" $f
-			fi
-			head -n1 $f  | sed -r 's/^/  /'
-		fi
-	fi
-
-	if tail -n+2 $f | egrep -q 'Copyright (\(C\)|\\\(co)' ; then
-
-		notice="`tail -n+2 $f | egrep 'Copyright (\(C\)|\\\(co)' | sed -r 's/^.*([0-9]{4})[^0-9]*$/\1/'`"
+	if egrep -q 'Copyright (\(C\)|\\\(co)' $f ; then
 		change="`hg log --no-merges --limit 1 --template '{date|isodate}' | egrep -o '^[0-9]+'`"
+		change_file="`hg log --no-merges --limit 1 --template '{date|isodate}' $f | egrep -o '^[0-9]+'`"
 
-		if [ "$notice" != "$change" ] ; then
-			echo "Updating Copyright notice inside $f:"
-			if tail -n+2 $f | egrep -q '[0-9]{4}-[0-9]{4}' ; then
-				sed -i -r "2,\$s/([0-9]{4}-)[0-9]{4}/\\1$change/" $f
+		first=y
+		egrep -n 'Copyright (\(C\)|\\\(co)' $f | sed 's/:/ /' | while read LINENR LINE ; do
+			if [ "$first" = y ] ; then
+				check_change="$change_file"
+				first=n
 			else
-				sed -i -r "2,\$s/([0-9]{4})([^0-9]+)/\\1-$change\\2/" $f
+				check_change="$change"
 			fi
-			tail -n+2 $f | egrep 'Copyright (\(C\)|\\\(co)' | sed -r 's/^/  /'
-		fi
+
+			notice="`echo \"$LINE\" | sed -r 's/^.*([0-9]{4})[^0-9]*$/\1/'`"
+
+			if [ "$notice" != "$check_change" ] ; then
+				echo "Updating Copyright notice in $f on line $LINENR:"
+				sed "${LINENR}!d" $f
+
+				want_update || continue
+
+				if echo "$LINE" | egrep -q '[0-9]{4}-[0-9]{4}[^0-9]+$' ; then
+					sed -i -r "${LINENR}s/([0-9]{4}-)[0-9]{4}([^0-9]+)$/\\1$check_change\\2/" $f
+				else
+					sed -i -r "${LINENR}s/([0-9]{4})([^0-9]+)$/\\1-$check_change\\2/" $f
+				fi
+				sed "${LINENR}!d" $f
+			fi
+		done
 	fi
 done
