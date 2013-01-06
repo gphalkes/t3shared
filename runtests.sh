28b0e5be890382aed72f53e734117c4b1dc1f3fd
Gertjan Halkes <hg@ghalkes.nl>
2013-01-06 15:41:27 +0100

    Add color to see what is happening

--- runtests.sh
+++ runtests.sh
@@ -9,6 +9,6 @@ fi
 cd `dirname "$0"`/..
 for i in $TESTS ; do
 	[ -x "$i/testsuite/runtests.sh" ] || continue
-	echo "==== Running testsuite for $i ===="
-	( cd "$i/testsuite" ; ./runtests.sh )
+	echo -e "\033[34;1m/// Running testsuite for $i ///\033[0m"
+	( cd "$i/testsuite" ; ./runtests.sh ) | sed -r '$s/(.*)/\x1b[35;1m\1\x1b[0m/'
 done
